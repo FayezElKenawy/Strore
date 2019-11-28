7f3594f9d6d57e4cf404c0d8b468401cc61a5430
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-28 10:03:40 +0300

    adding domain layer and context and define student class

---
+++ Store.Domain/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Domain.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ Store.Infra.Data/StoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Infra.Data
+{
+    public class StoreDbContext:DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
