74a5f97e39c38e59107de8c5d201eb755b225c2d
Fayez Emad Elkenawy <fezoelkenawy24@yahoo.com>
2019-11-28 14:14:07 +0300

    adding controller and view

---
+++ Store.PresentationLayer/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Store.Application.Interfaces;
+using Store.Application.ViewModels;
+
+namespace Store.PresentationLayer.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IStudentService _service;
+        public StudentController(IStudentService serviec)
+        {
+            _service = serviec;
+        }
+        public IActionResult Index()
+        {
+            StudentViewModel model = _service.GetStudents();
+            return View(model);
+        }
+    }
+}
